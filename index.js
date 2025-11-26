38dd89225ef8b4fe50a607c6b7a8e93fe777fc97
sentinel-rishi <sentinel@glimpass.com>
2025-11-26 11:06:57 +0000

    api memory feature

--- index.js
+++ index.js
@@ -253,7 +253,7 @@ app.use('/plivo', plivoRouter);
 app.use('/exotel', exotelRouter);
 app.use('/ip', ipRouter);
 app.use('/health', healthRouter);
-app.use('/', apiKeyValidator, plivoApiRouter)
+app.use('/api', apiKeyValidator, plivoApiRouter)
 
 // MarkAible AI Service Routes (using exact original endpoints)
 app.use('/api/create-ai', markaibleAiRouter);
